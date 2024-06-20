f99b7fdde61f79c22bdcaab56ba11031b3773920
Emma <emmauel1997@mail.com>
2024-06-20 10:22:00 -0500

    Initial Commit

---
+++ tca/tcaApp.swift
@@ -0,0 +1,17 @@
+//
+//  tcaApp.swift
+//  tca
+//
+//  Created by Emmanuel Zambrano on 20/06/24.
+//
+
+import SwiftUI
+
+@main
+struct tcaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ tcaUITests/tcaUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  tcaUITestsLaunchTests.swift
+//  tcaUITests
+//
+//  Created by Emmanuel Zambrano on 20/06/24.
+//
+
+import XCTest
+
+final class tcaUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
